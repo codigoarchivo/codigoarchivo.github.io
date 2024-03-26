6e24ea302c75f6713d6e78b5b2e2c8d75cedcc55
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-03-26 08:10:48 -0400

    add files static

--- src/interface/lang.ts
+++
@@ -1,1 +0,0 @@
-export type lang = 'en' | 'es'
--- src/lib/intl.ts
+++
@@ -1,9 +0,0 @@
-import { Locale } from "../../i18n-config";
-
-export async function getIntl(locale: Locale) {
-  return {
-    locale: locale,
-    messages: (await import(`@/lang/${locale}.json`)).default,
-  };
-}
-
