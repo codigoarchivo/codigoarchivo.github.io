d6d45cf5a9b4a4e026e608d660d88e821febf575
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-10-25 15:34:00 -0400

    refactor: reorganize the structure of new static files

--- static/RUfK6G9lF561uugofTRhG/buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/771-196fe9b12c959eff.js","static/chunks/pages/index-a49a7018dea7dff5.js"],"/_error":["static/chunks/pages/_error-5a00309fd5f4b49e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
